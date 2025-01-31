d8abca927f735d07fd4e7c7bad23641f0383a2e8
MJ <126430789+Ocariza23@users.noreply.github.com>
2025-01-31 21:50:58 +0800

    Gym Membership System

---
+++ GymMembershipSystem/src/main/java/com/mycompany/gymmembershipsystem/GymMembershipSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gymmembershipsystem;
+
+/**
+ *
+ * @author MJ
+ */
+public class GymMembershipSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

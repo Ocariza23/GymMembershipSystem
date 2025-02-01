bf2cd7430b4f65443a22f309c8c97a8e9bc98815
MJ <126430789+Ocariza23@users.noreply.github.com>
2025-02-01 13:06:54 +0800

    Added gymbro package

---
+++ GymMembershipSystem/src/main/java/com/gymmembership/system/gymbro/DashboardMembers.java
@@ -0,0 +1,9 @@
+package com.gymmembership.system.gymbro;
+
+import com.app.util.Function;
+
+public class DashboardMembers extends Function{
+    public void dashMembers() {
+            System.out.println("MEMBER");
+    }
+}
